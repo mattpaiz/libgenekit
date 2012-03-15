a6e0cc267e96c0bcefc5faed66755ab61bc2f657
Matthew Paiz <matt.paiz@gmail.com>
2012-03-14 18:20:21 -0700

    Added Licensing Information (v0.0.1)

--- src/chromosome.h
+++ src/chromosome.h
@@ -1,3 +1,21 @@
+/*
+ * chromosome.h
+ *
+ * Copyright 2012 Matthew Paiz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef __CHROMOSOME_H__
 #define __CHROMOSOME_H__
 
@@ -18,6 +36,5 @@ float gk_chromosome_get_fitness(gk_chromosome *);
 void gk_chromosome_set_equation(gk_chromosome *, char *);
 void gk_chromosome_set_fitness(gk_chromosome *c, float fitness);
 int gk_chromosome_set_node(gk_chromosome *c, gk_tree *node);
-
 
 #endif
